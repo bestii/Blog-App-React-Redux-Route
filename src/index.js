5345bf3dff40e8900caad3f6bb40f3434b04988c
Bestin <bestinjohn1993@gmail.com>
2017-08-26 16:15:29 +0530

    Added New Route

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route  } from 'react-router-dom';
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 // BrowserRouter Interacts with the history library
 import promise from 'redux-promise';
 import reducers from './reducers';
 import PostIndex from './components/posts_index';
-
+import PostNew from './components/post_new';
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-
+// '/'--->matches any route that starts with /
+/* Switch component is used to render a single component out of a number of components 
+   that match the route based on priority(the one that comes first has higher prriority)
+*/
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostIndex} />     
+        <Switch>
+          <Route path="/posts/new" component={PostNew} /> 
+          <Route path="/" component={PostIndex} />   
+        </Switch>    
       </div>
     </BrowserRouter>
   </Provider>
