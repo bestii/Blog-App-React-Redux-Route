66c11a3669d792254ba407697283f6a01e2e1040
Bestin <bestinjohn1993@gmail.com>
2017-08-26 20:01:32 +0530

    Created title field and corrected typo

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 class PostNew extends Component {
+    // {...field.input} is used to wireup the event handllers to the input its a short hand for onChange=field.input.onChange so on..
+    renderTitleField(field){
+        return (
+            <div className="form-group">
+                <label>Title</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+    // The field attribute doesn't know how to render UI for the element it only checks the under the hood functionalities
+    // It calls a function that contains a JSX blob to render the UI(this function is automically called so we dont hav eto call it explicitly)
     render() {
         return (
             <form>
                 <Field
                     name="title"
-                    component={}
+                    component={this.renderTitleField}
                 />
             </form>
         );
@@ -14,6 +29,6 @@ class PostNew extends Component {
 }
 
 export default reduxForm({
-                    // Unique name for our form
-                    form: 'PostNewForm'
+    // Unique name for our form
+    form: 'PostNewForm'
 })(PostNew);
