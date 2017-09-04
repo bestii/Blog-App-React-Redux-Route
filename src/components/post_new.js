0449d502ddee7edc01b587834d2aec6d0c11ba3c
Bestin <bestinjohn1993@gmail.com>
2017-09-05 00:25:10 +0530

    Added code to submit the form when all validation is satisfied

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -11,14 +11,22 @@ class PostNew extends Component {
                     type="text"
                     {...field.input}
                 />
+                {field.meta.error}
             </div>
         );
+    }
+    onSubmit(values) {
+        console.log(values)
     }
     // The field attribute doesn't know how to render UI for the element it only checks the under the hood functionalities
     // It calls a function that contains a JSX blob to render the UI(this function is automically called so we dont hav eto call it explicitly)
     render() {
+        // Reduxform returns a handler function after wiring up this function performs the redux side of validation and other stuff
+        const { handleSubmit } = this.props;
         return (
-            <form>
+            // onSubmit we are calling the handler function from ReduxForm which as a callback calls our onSubmit function if,
+            // everything is satisfied and the form is ready to submit.
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title"
                     name="title"
@@ -34,6 +42,7 @@ class PostNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
