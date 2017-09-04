943e35614df963751b896fbd3b296ba4c09b43cf
Bestin <bestinjohn1993@gmail.com>
2017-09-04 23:42:48 +0530

    Added validation helper function

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 class PostNew extends Component {
     // {...field.input} is used to wireup the event handllers to the input its a short hand for onChange=field.input.onChange so on..
-    renderField(field){
+    renderField(field) {
         return (
             <div className="form-group">
                 <label>{field.label}</label>
@@ -26,7 +26,7 @@ class PostNew extends Component {
                 />
                 <Field
                     label="Categories"
-                    name="tag"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -39,7 +39,26 @@ class PostNew extends Component {
     }
 }
 
+function validate(values) {
+    // values --> { title:'value', categories:'value', content:'value'}
+    var errors = {};
+    //validate the input from the values
+    if (!values.title) {
+        errors.title = "Please enter a title!";
+    }
+    if (!values.categories) {
+        errors.categories = "Please enter a categories!";
+    }
+    if (!values.content) {
+        errors.content = "Please enter a content!";
+    }
+    //if erros object is empty, fine to submit
+    //if errors object has any values redux assumes form is inavlid and not fine to submit
+    return errors;
+};
+
 export default reduxForm({
+    validate,
     // Unique name for our form
     form: 'PostNewForm'
 })(PostNew);
