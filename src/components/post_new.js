054c0f147640e77e304484242c2a482c1f09860c
Bestin <bestinjohn1993@gmail.com>
2017-09-05 23:39:42 +0530

    Created cancel Route and added style

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,58 +1,60 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 class PostNew extends Component {
-    // {...field.input} is used to wireup the event handllers to the input its a short hand for onChange=field.input.onChange so on..
-    renderField(field) {
-        // Destructuring using ES6
-        const { meta: { touched, error } } = field;
-        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
-        return (
-            <div className={className}>
-                <label>{field.label}</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    {...field.input}
-                />
-                <div className="text-help">
-                    {touched ? error : ''}
-                </div>
+        // {...field.input} is used to wireup the event handllers to the input its a short hand for onChange=field.input.onChange so on..
+        renderField(field) {
+            // Destructuring using ES6
+            const { meta: { touched, error } } = field;
+            const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+            return (
+                <div className={className}>
+                    <label>{field.label}</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        {...field.input}
+                    />
+                    <div className="text-help">
+                        {touched ? error : ''}
+                    </div>
 
-            </div>
-        );
+                </div>
+            );
+        }
+        onSubmit(values) {
+            console.log(values)
+        }
+        // The field attribute doesn't know how to render UI for the element it only checks the under the hood functionalities
+        // It calls a function that contains a JSX blob to render the UI(this function is automically called so we dont hav eto call it explicitly)
+        render() {
+            // Reduxform returns a handler function after wiring up this function performs the redux side of validation and other stuff
+            const { handleSubmit } = this.props;
+            return (
+                // onSubmit we are calling the handler function from ReduxForm which as a callback calls our onSubmit function if,
+                // everything is satisfied and the form is ready to submit.
+                <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                    <Field
+                        label="Title"
+                        name="title"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Categories"
+                        name="categories"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Post Content"
+                        name="content"
+                        component={this.renderField}
+                    />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <Link to="/" className="btn btn-danger">Cancel</Link>
+                </form>
+            );
+        }
     }
-    onSubmit(values) {
-        console.log(values)
-    }
-    // The field attribute doesn't know how to render UI for the element it only checks the under the hood functionalities
-    // It calls a function that contains a JSX blob to render the UI(this function is automically called so we dont hav eto call it explicitly)
-    render() {
-        // Reduxform returns a handler function after wiring up this function performs the redux side of validation and other stuff
-        const { handleSubmit } = this.props;
-        return (
-            // onSubmit we are calling the handler function from ReduxForm which as a callback calls our onSubmit function if,
-            // everything is satisfied and the form is ready to submit.
-            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <Field
-                    label="Title"
-                    name="title"
-                    component={this.renderField}
-                />
-                <Field
-                    label="Categories"
-                    name="categories"
-                    component={this.renderField}
-                />
-                <Field
-                    label="Post Content"
-                    name="content"
-                    component={this.renderField}
-                />
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        );
-    }
-}
 /*
  *  Three different states of a reduxForm
  *  Pristine --> Initial state right after load when user has not touched any of the form elements.
