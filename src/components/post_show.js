8f5ea7b0c11a59fb134b470b8fa1fd8b263d491f
Bestin <bestinjohn1993@gmail.com>
2017-10-01 00:29:14 +0530

    Added Code to render individual components

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../action';
+import { fetchPost } from '../actions';
 class PostShow extends Component {
     componentDidMount() {
         // React router passes URL param information as the props to the component that is rendered on that route.
         const { id } = this.props.match.params;
         this.props.fetchPost(id);
     }
+    
     render() {
+        const { post } = this.props;
+
+        /* Initially when the component renders post will be undefined 
+           since componentDidMount is executed after first render */
+        if(!post){
+            return (
+                <div>
+                    Loading....
+                </div>
+            );
+        }
+
         return (
             <div>
-                Show Post!
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
